3054c9c09dd56baed7343854c94a9ed7f98e2e5e
ali.sheharyar <ali.sheharyar@tamu.edu>
2020-09-18 19:44:17 +0300

    Updates

---
+++ precache-manifest.3fe6bdb548d0a1be880925e7d6c15a1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0f1804cb0bf9bfbeeab2c1afd63b99d",
+    "url": "/alisheharyar/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "631b69c6c62fc5c6ae16",
+    "url": "/alisheharyar/monsters-rolodex/static/css/main.4c418861.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/alisheharyar/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/alisheharyar/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "631b69c6c62fc5c6ae16",
+    "url": "/alisheharyar/monsters-rolodex/static/js/main.e277c8c0.chunk.js"
+  },
+  {
+    "revision": "013f5b22b2523250edbc",
+    "url": "/alisheharyar/monsters-rolodex/static/js/runtime-main.1f98fbe0.js"
+  }
+]);
